9e59efb0c17f131df0e0d87ff3ec184cc4173dc1
Yura Boyko <yurssoft@gmail.com>
2015-11-14 18:28:45 +0200

    Conference details

--- GDGTernopilSpeakersManager/SMConferenceDetailsViewController.m
+++ GDGTernopilSpeakersManager/SMConferenceDetailsViewController.m
@@ -7,7 +7,30 @@
 //
 
 #import "SMConferenceDetailsViewController.h"
+#import "SMConference.h"
+
+@interface SMConferenceDetailsViewController ()
+
+@property (strong, nonatomic) SMConference *conference;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *placeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
 
 @implementation SMConferenceDetailsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.titleLabel.text = self.conference.title;
+    self.placeLabel.text = self.conference.place;
+    self.dateLabel.text = self.conference.date.description;
+}
+
+- (void)setTheConferenceForDetails:(SMConference *)conferenceForDetails
+{
+    self.conference = conferenceForDetails;
+}
 
 @end
